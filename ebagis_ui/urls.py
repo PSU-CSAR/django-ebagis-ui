c1f050e768c5cd9853b07996eb53c54cd0f7fcf6
jkeifer <jarrettkeifer@gmail.com>
2016-10-30 11:47:36 -0700

    Update 2016-10-30T18:47:34.352Z

--- ebagis_ui/urls.py
+++ ebagis_ui/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 
-urlpatterns = (
+urlpatterns = [
     url(r'^', TemplateView.as_view(template_name='index.html')),
-)
+]
